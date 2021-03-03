db7e12ddb929cbfc6c6092ee6180849690c253df
Kirill <mrbomberman99@gmail.com>
2021-03-03 23:23:12 +0300

    added useState and useEffect to itemList.js

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,106 +1,80 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner/spinner';
-import ErrorMessage from '../errorMessage/errorMessage';
-import PropTypes from 'prop-types';
+// import ErrorMessage from '../errorMessage/errorMessage';
 // здесь мы собираем список персонажей
 
+function ItemList ({getData, onItemSelected, renderItem})  {
 
-class ItemList extends Component {
+    const [itemList , updateList] = useState([]);
 
-
-    static defaultProps = {
-        onItemSelected: () => {}
-    }
-    
-    static propTypes = {
-        onItemSelected: PropTypes.func, // проверяем функция ли это
-        // getData: PropTypes.arrayOf(PropTypes.object) // в getData должен быть массив, который состоит из объектов
-    }
-
+    useEffect(() => {
+        getData() // функция с уровня выше
+        .then((data) => {
+                updateList(data) // изменяем стейт, который лежит в itemList
+            })
+    }, []) // принимает второй аргумент, что делать, если стейт не изменился
+    // передаем пустой массив - это говорит хуку, что эффект необходимо выполнить только при появлении или исчезновении компонента
 
 
-    componentDidCatch(){
-        this.setState({
-            data: null,
-            error: true
-        })
-    }
+    // componentDidCatch(){
+    //     this.setState({
+    //         itemList: null,
+    //         error: true
+    //     })
+    // }
 
-    onError(status){ // передаем статус ошибки
-        this.setState({
-            data: null,
-            error: true
-        })
-    }
+    // onError(status){ // передаем статус ошибки
+    //     this.setState({
+    //         itemList: null,
+    //         error: true
+    //     })
+    // }
 
-    renderItems(arr){
+    function renderItems(arr){
         return arr.map((item) => { // перебираем массив объектов - это и есть те объекты, которые мы помещаем на страницу
             const {id} = item;  //  вытаскиваем свойства из каждого объекта
             // в верстке используем имя каждого объекта и его id, чтобы указывать, куда мы кликаем
-            const label = this.props.renderItem(item); // в нашу функцию из пропса передаем объект со свойтвом, которое интересно нам    
+            const label = renderItem(item); // в нашу функцию из пропса передаем объект со свойтвом, которое интересно нам    
             return (
                     <a class="list-group-item list-group-item-action" 
                     key={id} data-bs-toggle="list" href="#list-profile" 
                     role="tab" aria-controls="profile" 
-                    onClick={() => this.props.onItemSelected(id)}>{label}
+                    onClick={() => onItemSelected(id)}>{label}
                     </a>
             )
         })
     }
+//     // <a class="list-group-item list-group-item-action" 
+//     // id={i} data-bs-toggle="list" href="#list-profile" 
+//     // role="tab" aria-controls="profile" 
+//     // onClick={() => this.props.onCharSelected(i)}>{item.name}
+//     // </a>
+
+// /* <li 
+// key={id}
+// className="list-group-item" 
+// onClick={() => this.props.onCharSelected(id)}>{name}
+// </li>  */
+
+    
 
 
-    render() {
+        // if (error){
+        //     return <ErrorMessage/>
+        // }
 
-        const {data} = this.props;
-        const items = this.renderItems(data);
+        if(!itemList){
+            return <Spinner/>
+        }
+
+        const items = renderItems(itemList);
 
         return (
              <div class="list-group" id="list-tab" role="tablist">
                  {items}
             </div>
         );
-    }
-
-
 }
 
-const withData = (View) => {
-    return class extends Component {
-
-        state = {
-            data: null,
-            error: false
-        }
-    
-
-        componentDidMount(){
-            const {getData} = this.props // получаем функцию с уровня выше getAllCharacters  в данной ситуации
-    
-            getData() // функция с уровня выше
-                .then((data) => {
-                    this.setState({ // получим список персонажей со страницы
-                        data,
-                        error: false
-                    });
-                })
-                .catch(() => {this.onError()});
-        }
-
-        render() {
-
-            const {data, error} = this.state;
-
-            if (error){
-                return <ErrorMessage/>
-            }
-    
-            if(!data){
-                return <Spinner/>
-            }
-
-            return <View {...this.props} data={data}/>
-        }
-    }
-}
-export default withData(ItemList);
+export default ItemList;
